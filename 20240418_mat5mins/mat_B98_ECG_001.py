67daf5566f258e76c1cd13e67809c089d782a3f5
Jeremia Imanuel Gesha Pungus <jeremiapungus@gmail.com>
2024-05-25 21:33:02 +0900

    ECG b98 stroop 5mins fix

--- 20240418_mat5mins/mat_B98_ECG_001.py
+++ 20240418_mat5mins/mat_B98_ECG_001.py
@@ -6,9 +6,9 @@ Created on Tue Nov 14 12:43:19 2023
 """
 
 import mne
+import os
 import numpy as np
 import scipy
-import os
 from scipy.fft import fft, fftfreq
 from matplotlib import pyplot as plt
 from pan_tompkins import pan_tompkins_qrs
@@ -20,12 +20,17 @@ os.chdir(directory_path)
 
 raw = mne.io.read_raw_brainvision("20240418_mat5mins/20240418_B98_jikken_0003.vhdr")
 
+# Reconstruct the original events from our Raw object
+events, event_ids = mne.events_from_annotations(raw)
+
 raw.set_channel_types({'ECG':'ecg'})
 raw.set_channel_types({'vEOG':'eog'})
 raw.set_channel_types({'hEOG':'eog'})
 
-raw_ecg = raw.copy().pick_types(eeg=False, eog=False, ecg=True).crop(tmin = 33.185, tmax = 926.273) #make a copy
+tmin = events[3,0]/fs
+tmax = events[-1,0]/fs
 
+raw_ecg = raw.copy().crop(tmin = tmin, tmax = tmax).pick_types(eeg=False, eog=False, ecg=True)
 
 mne_ecg, mne_time = raw_ecg[:]
 mne_ecg = mne_ecg.T
@@ -108,27 +113,13 @@ result = np.array(result)
 # Clip the x locations less than 0 (Learning Phase)
 result = result[result > 0]
 
-# Calculate the heart rate
-heartRate = (60*fs)/np.average(np.diff(result[1:]))
-print("Heart Rate",heartRate, "BPM")
-
-# Plotting whole ECG signal
-plt.figure(figsize = (20,4), dpi = 100)
-plt.xticks(np.arange(0,len(mne_ecg)+1, 500))
-plt.plot(mne_ecg[:])
-plt.axvline(x = 300000, color = 'r')
-plt.axvline(x = 600000, color = 'r')
-plt.xlabel('Samples')
-plt.ylabel('mV')
-plt.title("Raw Signal")
-
 # Plotting the R peak locations in ECG signal
 plt.figure(figsize = (20,4), dpi = 100)
 plt.xticks(np.arange(0, len(mne_ecg)+1, 500))
 plt.plot(mne_ecg, color = 'blue')        
 plt.scatter(result, mne_ecg[result], color = 'red', s = 50, marker= '*')
-plt.axvline(x = 300000, color = 'r')
-plt.axvline(x = 600000, color = 'r')
+# plt.axvline(x = 300000, color = 'r')
+# plt.axvline(x = 600000, color = 'r')
 plt.xlabel('Samples')
 plt.ylabel('mV')
 plt.title("R Peak Locations")
@@ -144,61 +135,6 @@ new_r_peak = np.array(new_r_peak)
 r_peak = new_r_peak.copy()
 ###Pre Process, Data Cleaning ECG###
 
+result = r_peak.copy()
 rri = r_peak.copy()
 rri[1:] = rri[1:] - rri[:-1]
-
-
-#Interpolated Data
-exp_duration = len(mne_ecg)/fs
-
-x_vals = np.linspace(r_peak[0],r_peak[-1],int(exp_duration*fs))
-splines = scipy.interpolate.splrep(r_peak,rri)
-y_vals = scipy.interpolate.splev(x_vals,splines)
-
-plt.figure(figsize = (20,4),dpi = 100)
-plt.plot(x_vals/1000,y_vals)
-plt.axvline(x = 300, color = 'r')
-plt.axvline(x = 600, color = 'r')
-plt.title('Interpolated RRI')
-plt.ylabel('RRI [ms]')
-plt.xlabel('Time [sec]')
-
-#Data grouping
-y_firstrest = y_vals[0:300000]
-y_stress = y_vals[300001:600000]
-y_secondrest = y_vals[600001:]
-
-#Spectrogram RRI
-nperseg = 100000
-noverlap = nperseg*2/3
-f1,t1,Sxx = scipy.signal.spectrogram(y_vals, fs=fs, nperseg = nperseg, noverlap = noverlap, scaling='spectrum')
-plt.figure(figsize = (20,4), dpi = 100)
-plt.pcolormesh(t1, f1, np.abs(Sxx), shading='gouraud', snap= True)
-plt.title('Spectrogram')
-plt.ylabel('Frequency [Hz]')
-plt.xlabel('Time [sec]')
-plt.axis([t1[0], t1[-1], 0, 0.4])
-plt.axvline(x = 300, color = 'r')
-plt.axvline(x = 600, color = 'r')
-plt.colorbar()
-plt.show()
-
-#Measure SDNN
-sdnn_rest_1 = np.std(y_firstrest)
-sdnn_test = np.std(y_stress)
-sdnn_rest_2 = np.std(y_secondrest)
-print("Standard Deviation during First Rest: ", sdnn_rest_1)
-print("Standard Deviation during CSWT: ", sdnn_test)
-print("Standard Deviation during Second Rest: ", sdnn_rest_2)
-
-#Applying Fourier Transform
-time_step = np.average(x_vals[1:] -  x_vals[0:-1])
-
-sample_freq = fftfreq(y_vals.size, d=time_step)
-y = fft(y_vals)
-A = np.abs(y)
-
-plt.figure(figsize = (20,4),dpi = 100)
-plt.plot(sample_freq,y)
-
-#
