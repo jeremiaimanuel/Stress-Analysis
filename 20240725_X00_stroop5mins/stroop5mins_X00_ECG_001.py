0f88d70c997c27d1b65724d127d1255f3d7fe598
Jeremia Imanuel Gesha Pungus <jeremiapungus@gmail.com>
2024-09-25 18:14:04 +0900

    testing different detector

--- 20240725_X00_stroop5mins/stroop5mins_X00_ECG_001.py
+++ 20240725_X00_stroop5mins/stroop5mins_X00_ECG_001.py
@@ -199,3 +199,109 @@ for i in range(len(result)):
 r_peak_anot = mne.Annotations(r_peak_onset, 0.1, 'R Peak')
 
 raw_ecg.set_annotations(r_peak_anot)
+
+
+################# Testing Different Detector (NOT WORKING) #############
+def pan_tompkins_detector(unfiltered_ecg):
+    """
+    Jiapu Pan and Willis J. Tompkins.
+    A Real-Time QRS Detection Algorithm. 
+    In: IEEE Transactions on Biomedical Engineering 
+    BME-32.3 (1985), pp. 230–236.
+    """
+    
+    maxQRSduration = 0.150 #sec
+    f1 = 5/fs
+    f2 = 15/fs
+
+    b, a = signal.butter(1, [f1*2, f2*2], btype='bandpass')
+
+    filtered_ecg = signal.lfilter(b, a, unfiltered_ecg)        
+
+    diff = np.diff(filtered_ecg) 
+
+    squared = diff*diff
+
+    N = int(maxQRSduration*fs)
+    mwa = MWA_cumulative(squared, N)
+    mwa[:int(maxQRSduration*fs*2)] = 0
+
+    mwa_peaks = panPeakDetect(mwa, fs)
+
+    return mwa_peaks
+
+#Fast implementation of moving window average with numpy's cumsum function 
+def MWA_cumulative(input_array, window_size):
+    
+    ret = np.cumsum(input_array, dtype=float)
+    ret[window_size:] = ret[window_size:] - ret[:-window_size]
+    
+    for i in range(1,window_size):
+        ret[i-1] = ret[i-1] / i
+    ret[window_size - 1:]  = ret[window_size - 1:] / window_size
+    
+    return ret
+
+def panPeakDetect(detection, fs):    
+
+    min_distance = int(0.25*fs)
+
+    signal_peaks = [0]
+    noise_peaks = []
+
+    SPKI = 0.0
+    NPKI = 0.0
+
+    threshold_I1 = 0.0
+    threshold_I2 = 0.0
+
+    RR_missed = 0
+    index = 0
+    indexes = []
+
+    missed_peaks = []
+    peaks = []
+
+    for i in range(1,len(detection)-1):
+        if detection[i-1]<detection[i] and detection[i+1]<detection[i]:
+            peak = i
+            peaks.append(i)
+
+            if detection[peak]>threshold_I1 and (peak-signal_peaks[-1])>0.3*fs:
+                    
+                signal_peaks.append(peak)
+                indexes.append(index)
+                SPKI = 0.125*detection[signal_peaks[-1]] + 0.875*SPKI
+                if RR_missed!=0:
+                    if signal_peaks[-1]-signal_peaks[-2]>RR_missed:
+                        missed_section_peaks = peaks[indexes[-2]+1:indexes[-1]]
+                        missed_section_peaks2 = []
+                        for missed_peak in missed_section_peaks:
+                            if missed_peak-signal_peaks[-2]>min_distance and signal_peaks[-1]-missed_peak>min_distance and detection[missed_peak]>threshold_I2:
+                                missed_section_peaks2.append(missed_peak)
+
+                        if len(missed_section_peaks2)>0:
+                            signal_missed = [detection[i] for i in missed_section_peaks2]
+                            index_max = np.argmax(signal_missed)
+                            missed_peak = missed_section_peaks2[index_max]
+                            missed_peaks.append(missed_peak)
+                            signal_peaks.append(signal_peaks[-1])
+                            signal_peaks[-2] = missed_peak   
+
+            else:
+                noise_peaks.append(peak)
+                NPKI = 0.125*detection[noise_peaks[-1]] + 0.875*NPKI
+
+            threshold_I1 = NPKI + 0.25*(SPKI-NPKI)
+            threshold_I2 = 0.5*threshold_I1
+
+            if len(signal_peaks)>8:
+                RR = np.diff(signal_peaks[-9:])
+                RR_ave = int(np.mean(RR))
+                RR_missed = int(1.66*RR_ave)
+
+            index = index+1      
+    
+    signal_peaks.pop(0)
+
+    return signal_peaks
