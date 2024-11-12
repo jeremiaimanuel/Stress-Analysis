46e993f1244055f76b693c1b7728faa81a31f09b
Jeremia Imanuel Gesha Pungus <jeremiapungus@gmail.com>
2024-11-12 17:13:08 +0900

    Update machine_learning_hep_epoch.py
    adding permutation test score, adding whether to filter the epoch 1-30Hz or not

--- machine_learning_hep_epoch.py
+++ machine_learning_hep_epoch.py
@@ -24,7 +24,8 @@ include_second_rest = False
 segmented = True
 
 only_twave = True #IF True, better make n_segment = 2, if Flase, n_segment = 8
-stats = 'std'
+stats = 'all' #option is avg, std, all
+filt_30 = True
 if only_twave:
     n_segment = 4
 else:
@@ -33,8 +34,8 @@ else:
 ##################### Define What I need in here #####################
 
 # fpath = 'epoch_data'
-# fpath = 'hep_asr'
-fpath = 'hep_ica_all'
+fpath = 'hep_asr'
+# fpath = 'hep_ica_all'
 
 rest1_data = [os.path.join(fpath,i) for i in os.listdir(fpath) if i.endswith('first-epo.fif')]
 stress_data = [os.path.join(fpath,i) for i in os.listdir(fpath) if i.endswith('stress-epo.fif')]
@@ -44,15 +45,26 @@ file_rest1 = {number: rest1_data[number] for number in range(len(rest1_data))}
 file_stress = {number: stress_data[number] for number in range(len(stress_data))}
 file_rest2 = {number: rest2_data[number] for number in range(len(rest2_data))}
 
-def load_epoch_rest1(fdata):
-    epoch = mne.read_epochs(rest1_data[fdata], preload=True)
-    return(epoch)
-def load_epoch_stress(fdata):
-    epoch = mne.read_epochs(stress_data[fdata], preload=True)
-    return(epoch)
-def load_epoch_rest2(fdata):
-    epoch = mne.read_epochs(rest2_data[fdata], preload=True)
-    return(epoch)
+if filt_30 == True:
+    def load_epoch_rest1(fdata):
+        epoch = mne.read_epochs(rest1_data[fdata], preload=True).filter(1,30)
+        return(epoch)
+    def load_epoch_stress(fdata):
+        epoch = mne.read_epochs(stress_data[fdata], preload=True).filter(1,30)
+        return(epoch)
+    def load_epoch_rest2(fdata):
+        epoch = mne.read_epochs(rest2_data[fdata], preload=True).filter(1,30)
+        return(epoch)
+else:
+    def load_epoch_rest1(fdata):
+        epoch = mne.read_epochs(rest1_data[fdata], preload=True)
+        return(epoch)
+    def load_epoch_stress(fdata):
+        epoch = mne.read_epochs(stress_data[fdata], preload=True)
+        return(epoch)
+    def load_epoch_rest2(fdata):
+        epoch = mne.read_epochs(rest2_data[fdata], preload=True)
+        return(epoch)
 
 print(file_rest1)
 fnum = int(input("Choose Data: "))
@@ -174,9 +186,10 @@ df_labels = labels.join(labels_str)
 df = df_labels.join(features)
 display(df)
 
-n_features = 5
-X_5 = df.sample(n_features, axis = 1, random_state=99)
-df_5features = labels_str.join(X_5)
+# n_features = 5
+# # X_5 = df.sample(n_features, axis = 1, random_state=99)
+# X_5 = df[['status','std.ch.F3','std.ch.F1','std.ch.Fz','std.ch.F2','std.ch.F6']] #For data num 0
+# df_5features = labels_str.join(X_5)
 
 X = df.filter(like='ch')
 y = np.ravel(df.filter(like='label'))
@@ -184,65 +197,65 @@ y = np.ravel(df.filter(like='label'))
 
 ###############################################################################
 
-from scipy.stats import ttest_ind
+# from scipy.stats import ttest_ind
 
-t_test_data = ttest_ind(feature_rest,feature_stress)
-print(t_test_data.pvalue)
+# t_test_data = ttest_ind(feature_rest,feature_stress)
+# print(t_test_data.pvalue)
 
-p_value = t_test_data.pvalue
+# p_value = t_test_data.pvalue
 
-p_value_data = np.hstack=([ch_names, np.squeeze(p_value)])
+# p_value_data = np.vstack(np.array(([ch_names, np.squeeze(p_value)]))).T
 
 ###############################################################################
 from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import KFold, GridSearchCV, StratifiedKFold
 from sklearn.svm import LinearSVC
-from sklearn.model_selection import cross_val_score, cross_val_predict
+from sklearn.model_selection import cross_val_score, cross_val_predict,permutation_test_score
 from sklearn.metrics import classification_report, roc_auc_score, confusion_matrix, accuracy_score
 from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
 import umap.umap_ as mp
 import seaborn as sns
 ###############################################################################
 
-%matplotlib inline
+# %matplotlib inline
 
-sns.set(style='white', context='notebook', rc={'figure.figsize':(14,10)})
+# sns.set(style='white', context='notebook', rc={'figure.figsize':(14,10)})
 
-# sns.pairplot(df_5features, hue = 'label', palette= "tab10")
-sns.pairplot(df_5features, hue = 'status', palette= "tab10")
+# # sns.pairplot(df_5features, hue = 'label', palette= "tab10")
+# sns.pairplot(df_5features, hue = 'status', palette= "tab10")
 
-###############################################################################
+# ###############################################################################
 
-reducer = mp.UMAP()
-# reducer = mp.UMAP(n_neighbors=15, min_dist=0.1)
+# reducer = mp.UMAP()
+# # reducer = mp.UMAP(n_neighbors=15, min_dist=0.1)
 
-scaled_data = StandardScaler().fit_transform(X)
+# scaled_data = StandardScaler().fit_transform(X)
 
-embedding = reducer.fit_transform(scaled_data)
-embedding.shape
+# embedding = reducer.fit_transform(scaled_data)
+# embedding.shape
 
-unique_label = df.status.unique()
+# unique_label = df.status.unique()
 
-fig, ax = plt.subplots()
-scatter = ax.scatter(
-    embedding[:, 0],
-    embedding[:, 1],
-    c=[sns.color_palette()[x] for x in df.label])
+# fig, ax = plt.subplots()
+# scatter = ax.scatter(
+#     embedding[:, 0],
+#     embedding[:, 1],
+#     c=[sns.color_palette()[x] for x in df.label])
 
-# Create a custom legend with unique colors and labels
-legend_handles = []
-for i, label in enumerate(unique_label):
-    color = sns.color_palette()[i]
-    handle = plt.Line2D([0], [0], marker='o', color='w', label=label,
-                        markerfacecolor=color, markersize=10)
-    legend_handles.append(handle)
+# # Create a custom legend with unique colors and labels
+# legend_handles = []
+# for i, label in enumerate(unique_label):
+#     color = sns.color_palette()[i]
+#     handle = plt.Line2D([0], [0], marker='o', color='w', label=label,
+#                         markerfacecolor=color, markersize=10)
+#     legend_handles.append(handle)
 
-legend = ax.legend(handles=legend_handles, loc="lower left", title="Classes")
-ax.add_artist(legend)
+# legend = ax.legend(handles=legend_handles, loc="lower left", title="Classes")
+# ax.add_artist(legend)
 
-plt.gca().set_aspect('equal', 'datalim')
-plt.title('UMAP projection of the Dataset', fontsize=24)
+# plt.gca().set_aspect('equal', 'datalim')
+# plt.title('UMAP projection of the Dataset', fontsize=24)
 
 ################################ CLASSIFICATION 1 ################################
 
@@ -268,36 +281,39 @@ print("LDA ROC AUC Score: ", roc_scores.mean())
 y_pred = cross_val_predict(pipe, X,y, cv=skf)
 print(classification_report(y,y_pred))
 
-#######Plot LDA on each Fold#######
-fold_idx = 1
-for train_idx, test_idx in skf.split(X, y):
-    print(len(train_idx), len(test_idx))
-    
-    # Use .iloc[] for DataFrame and [] for numpy arrays
-    X_train, X_test = X.iloc[train_idx], X.iloc[test_idx]  # X is a pandas DataFrame
-    y_train, y_test = y[train_idx], y[test_idx]  # y is a numpy array
-    
-    # Fit the pipeline on the training set
-    pipe.fit(X_train, y_train)
-    
-    # Transform the data
-    X_train_lda = pipe.transform(X_train)
-    X_test_lda = pipe.transform(X_test)
-    
-    # Plot LDA for this fold
-    plt.figure()
-    for i in np.unique(y_train):
-        plt.scatter(X_train_lda[y_train == i], np.zeros_like(X_train_lda[y_train == i]), alpha=.8, label=f'Class {i} (train)')
-    for i in np.unique(y_test):
-        plt.scatter(X_test_lda[y_test == i], np.zeros_like(X_test_lda[y_test == i])-0.05, alpha=.8, label=f'Class {i} (test)')
-    
-    plt.legend(loc='best', shadow=False)
-    plt.yticks([])  # No need for y-axis ticks in 1D plot
-    plt.title(f'LDA of dataset for fold {fold_idx}')
-    plt.xlabel('Linear Discriminant')
-    plt.show()
+y_permut = permutation_test_score(pipe, X,y, cv=skf, random_state=99)
+print(y_permut)
 
-    fold_idx += 1
+#######Plot LDA on each Fold#######
+# fold_idx = 1
+# for train_idx, test_idx in skf.split(X, y):
+#     print(len(train_idx), len(test_idx))
+    
+#     # Use .iloc[] for DataFrame and [] for numpy arrays
+#     X_train, X_test = X.iloc[train_idx], X.iloc[test_idx]  # X is a pandas DataFrame
+#     y_train, y_test = y[train_idx], y[test_idx]  # y is a numpy array
+    
+#     # Fit the pipeline on the training set
+#     pipe.fit(X_train, y_train)
+    
+#     # Transform the data
+#     X_train_lda = pipe.transform(X_train)
+#     X_test_lda = pipe.transform(X_test)
+    
+#     # Plot LDA for this fold
+#     plt.figure()
+#     for i in np.unique(y_train):
+#         plt.scatter(X_train_lda[y_train == i], np.zeros_like(X_train_lda[y_train == i]), alpha=.8, label=f'Class {i} (train)')
+#     for i in np.unique(y_test):
+#         plt.scatter(X_test_lda[y_test == i], np.zeros_like(X_test_lda[y_test == i])-0.05, alpha=.8, label=f'Class {i} (test)')
+    
+#     plt.legend(loc='best', shadow=False)
+#     plt.yticks([])  # No need for y-axis ticks in 1D plot
+#     plt.title(f'LDA of dataset for fold {fold_idx}')
+#     plt.xlabel('Linear Discriminant')
+#     plt.show()
+
+#     fold_idx += 1
 #######Plot LDA on each Fold#######
 
 ################################ CLASSIFICATION 1 ################################
@@ -331,62 +347,65 @@ print("SVM ROC AUC Score: ", roc_scores.mean())
 y_pred = cross_val_predict(pipe, X,y, cv=skf)
 print(classification_report(y,y_pred))
 
+y_permut = permutation_test_score(pipe, X,y, cv=skf, random_state=99)
+print(y_permut)
+
 # cm = confusion_matrix(y,y_pred)
 # print(cm)
 
 #######Plot SVM#######
-from matplotlib.colors import ListedColormap
+# from matplotlib.colors import ListedColormap
 
-fold_idx = 1
-for train_idx, test_idx in skf.split(X, y):
-    print(len(train_idx), len(test_idx))
-    # Use .iloc[] for DataFrame and [] for numpy arrays
-    X_train, X_test = X.iloc[train_idx], X.iloc[test_idx]  # X is a pandas DataFrame
-    y_train, y_test = y[train_idx], y[test_idx]  # y is a numpy array
+# fold_idx = 1
+# for train_idx, test_idx in skf.split(X, y):
+#     print(len(train_idx), len(test_idx))
+#     # Use .iloc[] for DataFrame and [] for numpy arrays
+#     X_train, X_test = X.iloc[train_idx], X.iloc[test_idx]  # X is a pandas DataFrame
+#     y_train, y_test = y[train_idx], y[test_idx]  # y is a numpy array
     
-    #### Scaled and Classified 1 by 1 ####
-    X_train_scaled = scl.fit_transform(X_train,y_train)
-    X_test_scaled = scl.transform(X_test)
+#     #### Scaled and Classified 1 by 1 ####
+#     X_train_scaled = scl.fit_transform(X_train,y_train)
+#     X_test_scaled = scl.transform(X_test)
     
-    X_train_reduced = umap.fit_transform(X_train_scaled,y_train)
-    X_test_reduced = umap.transform(X_test_scaled)
+#     X_train_reduced = umap.fit_transform(X_train_scaled,y_train)
+#     X_test_reduced = umap.transform(X_test_scaled)
     
-    clf.fit(X_train_reduced, y_train)
+#     clf.fit(X_train_reduced, y_train)
 
-    x_min, x_max = X_test_reduced[:, 0].min() - 1, X_test_reduced[:, 0].max() + 1
-    y_min, y_max = X_test_reduced[:, 1].min() - 1, X_test_reduced[:, 1].max() + 1
-    xx, yy = np.meshgrid(np.arange(x_min, x_max, 0.02),
-                          np.arange(y_min, y_max, 0.02))
+#     x_min, x_max = X_test_reduced[:, 0].min() - 1, X_test_reduced[:, 0].max() + 1
+#     y_min, y_max = X_test_reduced[:, 1].min() - 1, X_test_reduced[:, 1].max() + 1
+#     xx, yy = np.meshgrid(np.arange(x_min, x_max, 0.02),
+#                           np.arange(y_min, y_max, 0.02))
 
-    grid = np.c_[xx.ravel(), yy.ravel()]
-    Z = np.reshape(clf.predict(grid), xx.shape)
+#     grid = np.c_[xx.ravel(), yy.ravel()]
+#     Z = np.reshape(clf.predict(grid), xx.shape)
 
-    plt.figure()
-    plt.contourf(xx, yy, Z, alpha=0.3,cmap = ListedColormap(('green','red')))
-    plt.xlim(xx.min(),xx.max())
-    plt.ylim(yy.min(),yy.max())
+#     plt.figure()
+#     plt.contourf(xx, yy, Z, alpha=0.3,cmap = ListedColormap(('green','red')))
+#     plt.xlim(xx.min(),xx.max())
+#     plt.ylim(yy.min(),yy.max())
 
-    for i,j in enumerate(np.unique(y_train)):
-        if j == 0:
-            label = 'Rest_train'
-        else:
-            label = 'Stress_train'
-        plt.scatter(X_train_reduced[y_train ==j,0],X_train_reduced[y_train==j,1],
-                    c=ListedColormap(('limegreen','lightcoral'))(i),label=label)
+#     for i,j in enumerate(np.unique(y_train)):
+#         if j == 0:
+#             label = 'Rest_train'
+#         else:
+#             label = 'Stress_train'
+#         plt.scatter(X_train_reduced[y_train ==j,0],X_train_reduced[y_train==j,1],
+#                     c=ListedColormap(('limegreen','lightcoral'))(i),label=label)
     
-    for i,j in enumerate(np.unique(y_test)):
-        if j == 0:
-            label = 'Rest_test'
-        else:
-            label = 'Stress_test'
-        plt.scatter(X_test_reduced[y_test ==j,0],X_test_reduced[y_test==j,1],
-                    c=ListedColormap(('green','red'))(i),label=label)
+#     for i,j in enumerate(np.unique(y_test)):
+#         if j == 0:
+#             label = 'Rest_test'
+#         else:
+#             label = 'Stress_test'
+#         plt.scatter(X_test_reduced[y_test ==j,0],X_test_reduced[y_test==j,1],
+#                     c=ListedColormap(('green','red'))(i),label=label)
     
-    plt.legend()
-    plt.title(f'Decision Boundary for Linear SVC fold {fold_idx}')
-    plt.show()
+#     plt.legend()
+#     plt.title(f'Decision Boundary for Linear SVC fold {fold_idx}')
+#     plt.show()
 
-    fold_idx+=1
+#     fold_idx+=1
 #######Plot SVM#######
 
 ################################ CLASSIFICATION 2 ################################
