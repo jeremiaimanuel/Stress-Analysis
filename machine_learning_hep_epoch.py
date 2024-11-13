7d8cc90f4fdef2ad97c29140942df0cbdafa2562
Jeremia Imanuel Gesha Pungus <74778890+jeremiaimanuel@users.noreply.github.com>
2024-11-14 08:30:13 +0900

    Update machine_learning_hep_epoch.py

--- machine_learning_hep_epoch.py
+++ machine_learning_hep_epoch.py
@@ -21,11 +21,11 @@ os.chdir(directory_path)
 ##################### Define What I need in here #####################
     
 include_second_rest = False
-segmented = True
+segmented = False
 
 only_twave = True #IF True, better make n_segment = 2, if Flase, n_segment = 8
-stats = 'all' #option is avg, std, all
-filt_30 = True
+stats = 'std' #option is avg, std, all
+filt_30 = False
 if only_twave:
     n_segment = 4
 else:
@@ -197,14 +197,14 @@ y = np.ravel(df.filter(like='label'))
 
 ###############################################################################
 
-# from scipy.stats import ttest_ind
+from scipy.stats import ttest_ind
 
-# t_test_data = ttest_ind(feature_rest,feature_stress)
-# print(t_test_data.pvalue)
+t_test_data = ttest_ind(feature_rest,feature_stress)
+print(t_test_data.pvalue)
 
-# p_value = t_test_data.pvalue
+p_value = t_test_data.pvalue
 
-# p_value_data = np.vstack(np.array(([ch_names, np.squeeze(p_value)]))).T
+p_value_data = np.vstack(np.array(([ch_names, np.squeeze(p_value)]))).T
 
 ###############################################################################
 from sklearn.pipeline import Pipeline
@@ -265,7 +265,9 @@ n_splits = 5
 # gkf = KFold(n_splits = n_splits)
 skf = StratifiedKFold(n_splits = n_splits)
 # tscv = TimeSeriesSplit(n_splits = n_splits)
-pipe = Pipeline([('scaler',StandardScaler()),('clf',clf)])
+# pipe = Pipeline([('scaler',StandardScaler()),('clf',clf)])
+umap = mp.UMAP(random_state=99)
+pipe = Pipeline([('scl',scl),('umap', umap),('clf',clf)])
 pipe.fit(X,y)
 
 acc_scores = cross_val_score(pipe, X, y, cv=skf, scoring='accuracy')
@@ -281,39 +283,39 @@ print("LDA ROC AUC Score: ", roc_scores.mean())
 y_pred = cross_val_predict(pipe, X,y, cv=skf)
 print(classification_report(y,y_pred))
 
-y_permut = permutation_test_score(pipe, X,y, cv=skf, random_state=99)
-print(y_permut)
+# y_permut = permutation_test_score(pipe, X,y, cv=skf, random_state=99)
+# print(y_permut)
 
 #######Plot LDA on each Fold#######
-# fold_idx = 1
-# for train_idx, test_idx in skf.split(X, y):
-#     print(len(train_idx), len(test_idx))
+fold_idx = 1
+for train_idx, test_idx in skf.split(X, y):
+    print(len(train_idx), len(test_idx))
     
-#     # Use .iloc[] for DataFrame and [] for numpy arrays
-#     X_train, X_test = X.iloc[train_idx], X.iloc[test_idx]  # X is a pandas DataFrame
-#     y_train, y_test = y[train_idx], y[test_idx]  # y is a numpy array
+    # Use .iloc[] for DataFrame and [] for numpy arrays
+    X_train, X_test = X.iloc[train_idx], X.iloc[test_idx]  # X is a pandas DataFrame
+    y_train, y_test = y[train_idx], y[test_idx]  # y is a numpy array
     
-#     # Fit the pipeline on the training set
-#     pipe.fit(X_train, y_train)
+    # Fit the pipeline on the training set
+    pipe.fit(X_train, y_train)
     
-#     # Transform the data
-#     X_train_lda = pipe.transform(X_train)
-#     X_test_lda = pipe.transform(X_test)
+    # Transform the data
+    X_train_lda = pipe.transform(X_train)
+    X_test_lda = pipe.transform(X_test)
     
-#     # Plot LDA for this fold
-#     plt.figure()
-#     for i in np.unique(y_train):
-#         plt.scatter(X_train_lda[y_train == i], np.zeros_like(X_train_lda[y_train == i]), alpha=.8, label=f'Class {i} (train)')
-#     for i in np.unique(y_test):
-#         plt.scatter(X_test_lda[y_test == i], np.zeros_like(X_test_lda[y_test == i])-0.05, alpha=.8, label=f'Class {i} (test)')
+    # Plot LDA for this fold
+    plt.figure()
+    for i in np.unique(y_train):
+        plt.scatter(X_train_lda[y_train == i], np.zeros_like(X_train_lda[y_train == i]), alpha=.8, label=f'Class {i} (train)')
+    for i in np.unique(y_test):
+        plt.scatter(X_test_lda[y_test == i], np.zeros_like(X_test_lda[y_test == i])-0.05, alpha=.8, label=f'Class {i} (test)')
     
-#     plt.legend(loc='best', shadow=False)
-#     plt.yticks([])  # No need for y-axis ticks in 1D plot
-#     plt.title(f'LDA of dataset for fold {fold_idx}')
-#     plt.xlabel('Linear Discriminant')
-#     plt.show()
+    plt.legend(loc='best', shadow=False)
+    plt.yticks([])  # No need for y-axis ticks in 1D plot
+    plt.title(f'LDA of dataset for fold {fold_idx}')
+    plt.xlabel('Linear Discriminant')
+    plt.show()
 
-#     fold_idx += 1
+    fold_idx += 1
 #######Plot LDA on each Fold#######
 
 ################################ CLASSIFICATION 1 ################################
@@ -347,65 +349,65 @@ print("SVM ROC AUC Score: ", roc_scores.mean())
 y_pred = cross_val_predict(pipe, X,y, cv=skf)
 print(classification_report(y,y_pred))
 
-y_permut = permutation_test_score(pipe, X,y, cv=skf, random_state=99)
-print(y_permut)
+# y_permut = permutation_test_score(pipe, X,y, cv=skf, random_state=99)
+# print(y_permut)
 
 # cm = confusion_matrix(y,y_pred)
 # print(cm)
 
 #######Plot SVM#######
-# from matplotlib.colors import ListedColormap
+from matplotlib.colors import ListedColormap
 
-# fold_idx = 1
-# for train_idx, test_idx in skf.split(X, y):
-#     print(len(train_idx), len(test_idx))
-#     # Use .iloc[] for DataFrame and [] for numpy arrays
-#     X_train, X_test = X.iloc[train_idx], X.iloc[test_idx]  # X is a pandas DataFrame
-#     y_train, y_test = y[train_idx], y[test_idx]  # y is a numpy array
+fold_idx = 1
+for train_idx, test_idx in skf.split(X, y):
+    print(len(train_idx), len(test_idx))
+    # Use .iloc[] for DataFrame and [] for numpy arrays
+    X_train, X_test = X.iloc[train_idx], X.iloc[test_idx]  # X is a pandas DataFrame
+    y_train, y_test = y[train_idx], y[test_idx]  # y is a numpy array
     
-#     #### Scaled and Classified 1 by 1 ####
-#     X_train_scaled = scl.fit_transform(X_train,y_train)
-#     X_test_scaled = scl.transform(X_test)
+    #### Scaled and Classified 1 by 1 ####
+    X_train_scaled = scl.fit_transform(X_train,y_train)
+    X_test_scaled = scl.transform(X_test)
     
-#     X_train_reduced = umap.fit_transform(X_train_scaled,y_train)
-#     X_test_reduced = umap.transform(X_test_scaled)
+    X_train_reduced = umap.fit_transform(X_train_scaled,y_train)
+    X_test_reduced = umap.transform(X_test_scaled)
     
-#     clf.fit(X_train_reduced, y_train)
+    clf.fit(X_train_reduced, y_train)
 
-#     x_min, x_max = X_test_reduced[:, 0].min() - 1, X_test_reduced[:, 0].max() + 1
-#     y_min, y_max = X_test_reduced[:, 1].min() - 1, X_test_reduced[:, 1].max() + 1
-#     xx, yy = np.meshgrid(np.arange(x_min, x_max, 0.02),
-#                           np.arange(y_min, y_max, 0.02))
+    x_min, x_max = X_test_reduced[:, 0].min() - 1, X_test_reduced[:, 0].max() + 1
+    y_min, y_max = X_test_reduced[:, 1].min() - 1, X_test_reduced[:, 1].max() + 1
+    xx, yy = np.meshgrid(np.arange(x_min, x_max, 0.02),
+                          np.arange(y_min, y_max, 0.02))
 
-#     grid = np.c_[xx.ravel(), yy.ravel()]
-#     Z = np.reshape(clf.predict(grid), xx.shape)
+    grid = np.c_[xx.ravel(), yy.ravel()]
+    Z = np.reshape(clf.predict(grid), xx.shape)
 
-#     plt.figure()
-#     plt.contourf(xx, yy, Z, alpha=0.3,cmap = ListedColormap(('green','red')))
-#     plt.xlim(xx.min(),xx.max())
-#     plt.ylim(yy.min(),yy.max())
+    plt.figure()
+    plt.contourf(xx, yy, Z, alpha=0.3,cmap = ListedColormap(('green','red')))
+    plt.xlim(xx.min(),xx.max())
+    plt.ylim(yy.min(),yy.max())
 
-#     for i,j in enumerate(np.unique(y_train)):
-#         if j == 0:
-#             label = 'Rest_train'
-#         else:
-#             label = 'Stress_train'
-#         plt.scatter(X_train_reduced[y_train ==j,0],X_train_reduced[y_train==j,1],
-#                     c=ListedColormap(('limegreen','lightcoral'))(i),label=label)
+    for i,j in enumerate(np.unique(y_train)):
+        if j == 0:
+            label = 'Rest_train'
+        else:
+            label = 'Stress_train'
+        plt.scatter(X_train_reduced[y_train ==j,0],X_train_reduced[y_train==j,1],
+                    c=ListedColormap(('limegreen','lightcoral'))(i),label=label)
     
-#     for i,j in enumerate(np.unique(y_test)):
-#         if j == 0:
-#             label = 'Rest_test'
-#         else:
-#             label = 'Stress_test'
-#         plt.scatter(X_test_reduced[y_test ==j,0],X_test_reduced[y_test==j,1],
-#                     c=ListedColormap(('green','red'))(i),label=label)
+    for i,j in enumerate(np.unique(y_test)):
+        if j == 0:
+            label = 'Rest_test'
+        else:
+            label = 'Stress_test'
+        plt.scatter(X_test_reduced[y_test ==j,0],X_test_reduced[y_test==j,1],
+                    c=ListedColormap(('green','red'))(i),label=label)
     
-#     plt.legend()
-#     plt.title(f'Decision Boundary for Linear SVC fold {fold_idx}')
-#     plt.show()
+    plt.legend()
+    plt.title(f'Decision Boundary for Linear SVC fold {fold_idx}')
+    plt.show()
 
-#     fold_idx+=1
+    fold_idx+=1
 #######Plot SVM#######
 
 ################################ CLASSIFICATION 2 ################################
